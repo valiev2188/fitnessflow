44aae6ea25be10701809251ed4b7f9e87185074b
admin <lola@MacBook-Pro-lola.local>
2026-03-17 00:14:06 +0500

    chore: update drizzle config for turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'drizzle-kit';
 import * as dotenv from 'dotenv';
-dotenv.config({ override: true });
+dotenv.config({ path: '.env.local', override: true });
 
 export default defineConfig({
   schema: './src/db/schema.ts',
   out: './src/db/migrations',
-  dialect: 'postgresql',
+  dialect: 'turso',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.TURSO_DB_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 });
